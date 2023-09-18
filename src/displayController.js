1d032620e724d99533d702523e43d0bd83d67f20
maurolps <boroengenharia@gmail.com>
2023-09-18 18:56:13 -0300

    UI update

---
+++ src/displayController.js
@@ -0,0 +1,10 @@
+export const startListener = () => {
+
+const btnMobileMenu = document.querySelector(".js-btn-mobile-menu");
+const siderBar = document.querySelector(".js-sidebar");
+
+btnMobileMenu.addEventListener("click", () => {
+  siderBar.classList.toggle("-translate-x-full");
+})
+
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import { loadProject } from './storage.js';
+import { startListener } from './ui.js';
 
 // const todoList = loadProject();
 // console.log(todoList);
+
+startListener();
+
