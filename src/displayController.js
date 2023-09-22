649bcf41fecd0e3957587d4c45e5c83e78321d2e
maurolps <boroengenharia@gmail.com>
2023-09-22 16:28:10 -0300

    Turn newCard function into factory

--- src/displayController.js
+++ src/displayController.js
@@ -1,4 +1,4 @@
-export const colors = {
+export const themeColors = {
   red: "#fecaca",
   lime: "#d9f99d",
   cyan: "#a5f3fc",
@@ -19,24 +19,27 @@ btnMobileMenu.addEventListener("click", () => {
 
 }
 
-export function newTask (taskContainer, taskText, color) {
-  const taskTemplate = document.getElementById('js-task-template');
-  const newTask = taskTemplate.content.cloneNode(true);
-  newTask.querySelector(".js-bordercolor").style.borderColor = color;
-  newTask.querySelector('.js-task-text').textContent = taskText;
-  taskContainer.appendChild(newTask);
-}
-
 export function newCard (title, color) {
   const cardContainer = document.getElementById('js-card-container');
   const cardTemplate = document.getElementById('js-card-template');
+  const taskTemplate = document.getElementById('js-task-template');
   
   const newTodoCard = cardTemplate.content.cloneNode(true);
   const taskContainer = newTodoCard.querySelector('.js-task-container');
   const cardTitle = newTodoCard.querySelector('.js-card-title');
 
-  cardTitle.style.backgroundColor = color;
+  const themeColor = themeColors[color] || themeColors.defaultColor;
+
+  cardTitle.style.backgroundColor = themeColor;
   cardTitle.textContent = title;
   cardContainer.appendChild(newTodoCard);
-  return taskContainer;
+
+  const newTask = (taskText) => {
+    const addTask = taskTemplate.content.cloneNode(true);
+    addTask.querySelector(".js-bordercolor").style.borderColor = themeColor;
+    addTask.querySelector('.js-task-text').textContent = taskText;
+    taskContainer.appendChild(addTask);
+  }
+
+  return {newTask};
 }
--- src/index.js
+++ src/index.js
@@ -1,34 +1,23 @@
 import { loadProject, saveProject } from './storage.js';
-import { startListener, newCard, newTask, colors } from './displayController.js';
+import { startListener, newCard } from './displayController.js';
 
 function loadTodoList (projectList, projectIndex) {
   const todo = projectList[projectIndex]["todo-list"];
 
   for (let i = 0; i < todo.length; i++) {
-    let color = colors[todo[i].color];
-    let taskContainer = newCard(todo[i].title, color);
+    const addCard = newCard(todo[i].title, todo[i].color);
     for (let j = 0; j < todo[i].tasks.length; j++) {
-      newTask(taskContainer, todo[i].tasks[j].task, color)
+      addCard.newTask(todo[i].tasks[j].task);
     }
   }
 }
 
 // saveProject()
 const projectList = loadProject();
-console.log(typeof(projectList)) 
 loadTodoList(projectList, 0);
 
-let taskContainer = newCard('Check it out 1', 'pink');
-newTask(taskContainer, 'My empty task goes here', 'pink');
-newTask(taskContainer, 'My empty task goes here', 'pink');
-newTask(taskContainer, 'My empty task goes here', 'pink');
-newTask(taskContainer, 'My empty task goes here', 'pink');
-newTask(taskContainer, 'My empty task goes here', 'pink');
-
-taskContainer = newCard('Check it out 2', 'lightblue');
-newTask(taskContainer, 'My empty task goes here', 'lightblue');
-newTask(taskContainer, 'My empty task goes here', 'lightblue');
-newTask(taskContainer, 'My empty task goes here', 'lightblue');
-newTask(taskContainer, 'My empty task goes here', 'lightblue');
+// const addCard = newCard("Example card", "cyan");
+// addCard.newTask('HelloWorld');
 
 // startListener();
+
