6cae597127e7029e91de56ca274639d39bebdbb1
maurolps <boroengenharia@gmail.com>
2023-09-21 21:08:30 -0300

    Added newTask function

--- src/displayController.js
+++ src/displayController.js
@@ -9,19 +9,24 @@ btnMobileMenu.addEventListener("click", () => {
 
 }
 
+export function newTask (taskContainer, taskText, color) {
+  const taskTemplate = document.getElementById('js-task-template');
+  const newTask = taskTemplate.content.cloneNode(true);
+  newTask.querySelector(".js-bordercolor").style.borderColor = color;
+  newTask.querySelector('.js-task-text').textContent = taskText;
+  taskContainer.appendChild(newTask);
+}
+
 export function newCard (title, color) {
   const cardContainer = document.getElementById('js-card-container');
   const cardTemplate = document.getElementById('js-card-template');
-  const taskTemplate = document.getElementById('js-task-template');
   
   const newTodoCard = cardTemplate.content.cloneNode(true);
-  const newTask = taskTemplate.content.cloneNode(true);
   const taskContainer = newTodoCard.querySelector('.js-task-container');
   const cardTitle = newTodoCard.querySelector('.js-card-title');
 
   cardTitle.style.backgroundColor = color;
   cardTitle.textContent = title;
-  newTask.querySelector(".js-bordercolor").style.borderColor = color;
-  taskContainer.appendChild(newTask);
   cardContainer.appendChild(newTodoCard);
+  return taskContainer;
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import { loadProject } from './storage.js';
-import { startListener, newCard } from './displayController.js';
+import { startListener, newCard, newTask } from './displayController.js';
 
 // const todoList = loadProject();
 // console.log(todoList);
 
 // startListener();
 
-newCard('Check it out 1', 'pink');
-newCard('Check it out 2', 'lightblue');
+let taskContainer = newCard('Check it out 1', 'pink');
+newTask(taskContainer, 'My empty task goes here', 'blue');
+
+taskContainer = newCard('Check it out 2', 'lightblue');
+newTask(taskContainer, 'My empty task goes here', 'pink');
