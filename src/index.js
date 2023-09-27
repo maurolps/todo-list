2e35031a089cf02750f49f1e2e7a43ce0e386c9f
maurolps <boroengenharia@gmail.com>
2023-09-26 23:52:14 -0300

    Made changes to support new functions

--- src/index.js
+++ src/index.js
@@ -1,25 +1,8 @@
-import { loadProjects } from './storage.js';
-import { docClickHandler, newCard, newProjectMenu } from './displayController.js';
+import { LoadProjects } from './storage.js';
+import { docClickHandler} from './displayController.js';
 
-function loadTodoList (projectList, projectIndex) {
-  const todo = projectList[projectIndex]["todo-list"];
-
-  for (let i = 0; i < todo.length; i++) {
-    const addCard = newCard(todo[i].title, todo[i].color);
-    for (let j = 0; j < todo[i].tasks.length; j++) {
-      addCard.newTask(todo[i].tasks[j].task);
-    }
-  }
-}
-
-function loadPrjList (projectList) {
-  for (let i = 0; i < projectList.length; i++) {
-    newProjectMenu(projectList[i].title);
-  }
-}
-
-const projectList = loadProjects();
-loadTodoList(projectList, 0);
+const projectList = LoadProjects();
+projectList.loadTodoList(0);
+projectList.loadMenu();
 docClickHandler();
-loadPrjList(projectList);
 
